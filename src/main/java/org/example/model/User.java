35224bc647e1b0ba0300b83e87d0449c391d5c76
Nhazz <manjirokys@gmail.com>
2026-09-18 19:46:49 +0700

    completly build a Transaction,Account,and User in model package

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,4 +1,38 @@
 package org.example.model;
 
 public class User {
+
+    private String fullName;
+    private String phoneNumber;
+    private int pin;
+
+    // default constructor
+    public User(){
+    }
+    public User(String fullName, String phoneNumber, int pin){
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.pin = pin;
+    }
+
+    // setter
+    public void setFullName(String fullName){
+        this.fullName = fullName;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public void setPin(int pin){
+        this.pin = pin;
+    }
+    // getter
+    public String getFullName(){
+        return fullName;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public int getPin(){
+        return pin;
+    }
 }

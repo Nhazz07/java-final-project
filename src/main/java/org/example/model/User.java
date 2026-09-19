9d4da4fd1b2f8fefbe48a22537f9b4e857f8fa2b
Nhazz <manjirokys@gmail.com>
2026-09-19 11:14:47 +0700

    updated account and user in model

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -2,6 +2,7 @@ package org.example.model;
 
 public class User {
 
+    private long id;
     private String fullName;
     private String phoneNumber;
     private int pin;
@@ -9,13 +10,16 @@ public class User {
     // default constructor
     public User(){
     }
-    public User(String fullName, String phoneNumber, int pin){
+    public User(long id, String fullName, String phoneNumber, int pin){
         this.fullName = fullName;
         this.phoneNumber = phoneNumber;
         this.pin = pin;
     }
 
     // setter
+    public void setId(long id){
+        this.id = id;
+    }
     public void setFullName(String fullName){
         this.fullName = fullName;
     }
@@ -26,6 +30,9 @@ public class User {
         this.pin = pin;
     }
     // getter
+    public long getId(){
+        return id;
+    }
     public String getFullName(){
         return fullName;
     }
@@ -35,4 +42,5 @@ public class User {
     public int getPin(){
         return pin;
     }
+
 }

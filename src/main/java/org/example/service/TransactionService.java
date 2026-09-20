0e83085c09ff0605716a0f0879c7c4f77b15147b
Sakhey Marvin <sakheymavin@gmail.com>
2026-09-20 21:49:39 +0700

    Added Transfer function and it's Methods

--- src/main/java/org/example/service/TransactionService.java
+++ src/main/java/org/example/service/TransactionService.java
@@ -9,4 +9,32 @@ import org.example.model.Account;
 import java.math.BigDecimal;
 import java.util.List;
 public class TransactionService {
+    private final AccountDao account_dao;
+    private final TransactionDao transaction_dao;
+    // Constructor
+
+    public TransactionService(AccountDao account_dao, TransactionDao transaction_dao) {
+        this.account_dao = account_dao;
+        this.transaction_dao = transaction_dao;
+    }
+
+    // Tranfer Function
+    public boolean transfer(long account_id, BigDecimal amount, String related_Acc) {
+        // Check amount
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            System.out.println("Transfer amount must be greater than 0.");
+            return false;
+        }
+        // check if reciever account is real or valid
+        if (related_Acc == null || related_Acc.isBlank()) {
+            System.out.println("Need a valid reciever account");
+            return false;
+        }
+        // Find the desired Sender account
+        Account sender_acc = account_dao.findById(account_id);
+        if (sender_acc == null) {
+            System.out.println("Sender is invalid account");
+            return false;
+        }
+    }
 }

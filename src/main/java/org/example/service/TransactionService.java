ffaf609b1a338866c3a46e738efa93d1723d5409
Sakhey Marvin <sakheymavin@gmail.com>
2026-09-21 08:59:15 +0700

    Completed TransactionService, Added Find,Updated,Record method  and Transaction History

--- src/main/java/org/example/service/TransactionService.java
+++ src/main/java/org/example/service/TransactionService.java
@@ -2,32 +2,35 @@ package org.example.service;
 import org.example.dao.TransactionDao;
 import org.example.dao.AccountDao;
 import org.example.model.Account;
+import org.example.model.Transaction;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 public class TransactionService {
     private final AccountDao account_dao;
     private final TransactionDao transaction_dao;
     // Constructor
 
-    public TransactionService(AccountDao account_dao, TransactionDao transaction_dao) {
-        this.account_dao = account_dao;
-        this.transaction_dao = transaction_dao;
+    public TransactionService(AccountDao accountDao, TransactionDao transactionDao) {
+        this.account_dao = accountDao;
+        this.transaction_dao = transactionDao;
     }
 
     // Transfer Function
-    public boolean transfer(long account_id, BigDecimal amount, String related_Acc) {
+    public boolean transfer(long accountId, BigDecimal amount, String relatedAcc) {
         // Check amount
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             System.out.println("Transfer amount must be greater than 0.");
             return false;
         }
         // check if receiver account is real or valid
-        if (related_Acc == null || related_Acc.isBlank()) {
+        if (relatedAcc == null || relatedAcc.isBlank()) {
             System.out.println("Need a valid receiver account");
             return false;
         }
         // Find the desired Sender account
-        Account sender_acc = account_dao.findById(account_id);
+        Account sender_acc = account_dao.findById(accountid);
         if (sender_acc == null) {
             System.out.println("Sender is invalid account");
             return false;
@@ -51,6 +54,41 @@ public class TransactionService {
         // Calculate new balance amount
         BigDecimal sender_acc_newBalance = sender_acc.getBalance().subtract(amount);
         BigDecimal receiver_acc_newBalance = receiver_acc.getBalance().add(amount);
+        // Update Sender Balance
+        boolean sender_acc_updated = account_dao.updateBalance(sender_acc.getId(), sender_acc_newBalance);
+        // check if sender_acc  is eligible to be update balance
+        if(!sender_acc_updated){
+            System.out.println("Error.");
+            return false;
+        }
+        //update receiver balance
+        boolean receiver_acc_updated = account_dao.updateBalance(receiver_acc.getId(), receiver_acc_newBalance);
+        // check if receiver_acc is eligible to be update balance
+        if(!receiver_acc_updated){
+            System.out.println("Error");
+            return false;
+        }
 
+        // Transaction Record
+        Transaction transactionRecord = new Transaction();
+        transactionRecord.setAccountId(sender_acc.getId());
+        transactionRecord.setType("TRANSFER");
+        transactionRecord.setAmount(amount);
+        transactionRecord.setRelatedAccount(receiver_acc.getAccountNumber());
+        // save those transaction record
+        boolean savedTransactionRecord = transaction_dao.save(transactionRecord);
+        // if fail
+        if(!savedTransactionRecord){
+            System.out.println("Transfer Unsuccessful");
+            return false;
+        }
+        // if it went successful
+        System.out.println("Transfer Successful");
+        return true;
+    }
+
+    // GetTransaction History
+    public List<Transaction> getTransactionHistory(long accountId){
+        return transaction_dao.findByAccountId(accountId);
     }
 }
